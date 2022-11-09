57b5bd14176ba1478b837c92b739b821ed89f210
Ravi Ranjan Kumar <ravirkumar2422@gmail.com>
2022-11-09 17:26:47 +0530

    fc

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://ravi:5iaGvy3weeV5WHxi@cluster0.yplrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  JWT_SECRET: "effegwegwrgrgw6",
+};

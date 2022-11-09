b4940f26bec0535039b8a7a813712b54b2219ad5
ravi-2422 <90566277+ravi-2422@users.noreply.github.com>
2022-11-09 17:28:21 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://ravi:5iaGvy3weeV5WHxi@cluster0.yplrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  JWT_SECRET: "effegwegwrgrgw6",
-};
